ad128d541554d7bfce34e2a513ab4dfd6b463a2b
Forest Anderson <forestkzanderson@gmail.com>
2019-10-20 14:04:12 -0400

    Added auto play videos

--- Scripts/TVComponents/Channel.gd
+++ Scripts/TVComponents/Channel.gd
@@ -2,6 +2,11 @@ extends Control
 
 func set_video_stream(video_stream: VideoStreamTheora):
 	$VideoPlayer.stream = video_stream
+	$VideoPlayer.connect("finished", self, "_on_VideoPlayer_finished")
+	
 
 func _ready():
 	pass # Replace with function body.
+
+func _on_VideoPlayer_finished():
+	$VideoPlayer.play()
